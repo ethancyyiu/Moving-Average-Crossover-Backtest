00b45694346ee60a41ec3317d2b44bdf8eea504e
Ethan Yiu <ethan.cy.yiu@gmail.com>
2026-04-30 23:10:58 -0700

    fixed multiindex columns

--- Main.py
+++ Main.py
@@ -3,10 +3,14 @@ import numpy as np
 import yfinance as yf
 import matplotlib.pyplot as plt
 
-# 1. Download data
 data = yf.download("SPY", start="2015-01-01", end="2025-01-01")
 
-print(data.columns)
+if isinstance(data.columns, pd.MultiIndex):
+    data.columns = data.columns.get_level_values(0)
+
+print(data.columns)  
+
+data["price"] = data["Close"]
 
 
 
